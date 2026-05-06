d5e0e80b9cacd3a2329b81f8f5919ce8546caaf5
= <jtnoble321@gmail.com>
2026-05-05 22:13:32 -0500

    add splash/icon, force light mode

--- MyCourse/MyCourse/App.xaml.cs
+++ MyCourse/MyCourse/App.xaml.cs
@@ -5,7 +5,7 @@
         public App()
         {
             InitializeComponent();
-
+            Application.Current.UserAppTheme = AppTheme.Light;
             MainPage = new NavigationPage(new MainPage());
         }
     }
